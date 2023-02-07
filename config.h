44a2c4bc13e64a0a03d161f888aeb4ce4497595c
Mattes Wenzel <mattes@flawlesslogic.net>
2023-02-07 22:26:57 +0100

    apply restart menu patch

--- config.h
+++ config.h
@@ -65,6 +65,8 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *termaltcmd[]  = { "urxvt", NULL };
 
+#include "exitdwm.c"
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -101,7 +103,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      exitdwm,           {0} },
 };
 
 /* button definitions */
