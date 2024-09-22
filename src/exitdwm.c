53bf1a890f3ae9a5e2a485151e8f2223448483c3
Mattes Wenzel <mattes@flawlesslogic.net>
2024-09-22 17:12:57 +0200

    Format code

--- src/exitdwm.c
+++ src/exitdwm.c
@@ -3,79 +3,73 @@
 #include <stdlib.h>
 #include <string.h>
 
-void exitdwm()
-{
-#if defined S_LOCK ||             \
-    defined S_RESTART_DWM ||      \
-    defined S_OFFSCREEN ||        \
-    defined S_EXIT ||             \
-    defined S_REBOOT ||           \
-    defined S_SHUTDOWN ||         \
-    defined S_LOCK_ICON ||        \
-    defined S_RESTART_DWM_ICON || \
-    defined S_OFFSCREEN_ICON ||   \
-    defined S_EXIT_ICON ||        \
-    defined S_REBOOT_ICON ||      \
-    defined S_SHUTDOWN_ICON ||    \
-    defined S_FORMAT ||           \
-    defined S_FORMAT_CLEAR
-#error(conflicting macro names)
-#endif
+void exitdwm(){
+  #if defined S_LOCK ||             \
+      defined S_RESTART_DWM ||      \
+      defined S_OFFSCREEN ||        \
+      defined S_EXIT ||             \
+      defined S_REBOOT ||           \
+      defined S_SHUTDOWN ||         \
+      defined S_LOCK_ICON ||        \
+      defined S_RESTART_DWM_ICON || \
+      defined S_OFFSCREEN_ICON ||   \
+      defined S_EXIT_ICON ||        \
+      defined S_REBOOT_ICON ||      \
+      defined S_SHUTDOWN_ICON ||    \
+      defined S_FORMAT ||           \
+      defined S_FORMAT_CLEAR
+  #error(conflicting macro names)
+  #endif
 
-#define S_LOCK "Lock"
-#define S_RESTART_DWM "restart Dwm"
-#define S_OFFSCREEN "Off-screen"
-#define S_EXIT "Exit"
-#define S_REBOOT "Reboot"
-#define S_SHUTDOWN "Shutdown"
-#define S_LOCK_ICON "\uf023" // <= FontAwesome icons
-#define S_RESTART_DWM_ICON "\uf01e"
-#define S_OFFSCREEN_ICON "\uf108"
-#define S_EXIT_ICON "\uf2f5"
-#define S_REBOOT_ICON "\uf021"
-#define S_SHUTDOWN_ICON "\uf011"
+  #define S_LOCK "Lock"
+  #define S_RESTART_DWM "restart Dwm"
+  #define S_OFFSCREEN "Off-screen"
+  #define S_EXIT "Exit"
+  #define S_REBOOT "Reboot"
+  #define S_SHUTDOWN "Shutdown"
+  #define S_LOCK_ICON "\uf023" // <= FontAwesome icons
+  #define S_RESTART_DWM_ICON "\uf01e"
+  #define S_OFFSCREEN_ICON "\uf108"
+  #define S_EXIT_ICON "\uf2f5"
+  #define S_REBOOT_ICON "\uf021"
+  #define S_SHUTDOWN_ICON "\uf011"
 
-#define S_FORMAT(ACTION) S_##ACTION##_ICON " " S_##ACTION
-#define S_FORMAT_CLEAR "sed 's/^..//'"
+  #define S_FORMAT(ACTION) S_##ACTION##_ICON " " S_##ACTION
+  #define S_FORMAT_CLEAR "sed 's/^..//'"
 
-  FILE *exit_menu = popen(
-      "echo \"" S_FORMAT(LOCK) "\n" S_FORMAT(RESTART_DWM) "\n" S_FORMAT(OFFSCREEN) "\n" S_FORMAT(EXIT) "\n" S_FORMAT(REBOOT) "\n" S_FORMAT(SHUTDOWN) "\" | dmenu -p exit: | " S_FORMAT_CLEAR,
-      "r");
+  char *a = "echo \"" S_FORMAT(LOCK) "\n" S_FORMAT(RESTART_DWM) "\n" S_FORMAT(OFFSCREEN) "\n" S_FORMAT(EXIT) "\n" S_FORMAT(REBOOT) "\n" S_FORMAT(SHUTDOWN) "\" | dmenu -p exit: | " S_FORMAT_CLEAR;
+  printf("command:\n%s\n", a);
+  FILE *exit_menu = popen(a, "r");
 
   char exit_action[16];
   int systemReturn;
 
-  if (
-      exit_menu == NULL ||
-      fscanf(exit_menu, "%15[a-zA-Z -]", exit_action) == EOF)
-  {
+  if (exit_menu == NULL || fscanf(exit_menu, "%15[a-zA-Z -]", exit_action) == EOF){
     fputs("Error. Failure in exit_dwm.", stderr);
     goto close_streams;
   }
 
-  if (strcmp(exit_action, S_LOCK) == 0)
-  {
+  if (strcmp(exit_action, S_LOCK) == 0){
     systemReturn = system("slock & sleep .5; xset dpms force off");
   }
 
-  if (strcmp(exit_action, S_RESTART_DWM) == 0)
-  {
+  if (strcmp(exit_action, S_RESTART_DWM) == 0){
     quit(&(const Arg){1});
   }
-  if (strcmp(exit_action, S_OFFSCREEN) == 0)
-  {
+
+  if (strcmp(exit_action, S_OFFSCREEN) == 0){
     systemReturn = system("sleep .5; xset dpms force off");
   }
-  if (strcmp(exit_action, S_EXIT) == 0)
-  {
+
+  if (strcmp(exit_action, S_EXIT) == 0){
     quit(&(const Arg){0});
   }
-  if (strcmp(exit_action, S_REBOOT) == 0)
-  {
+
+  if (strcmp(exit_action, S_REBOOT) == 0){
     systemReturn = system("systemctl reboot");
   }
-  if (strcmp(exit_action, S_SHUTDOWN) == 0)
-  {
+
+  if (strcmp(exit_action, S_SHUTDOWN) == 0){
     systemReturn = system("systemctl poweroff -i");
   }
 
