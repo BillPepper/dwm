20ee66a4bf364f2108ebf6658d53834eab9f921b
Mattes Wenzel <mattes@flawlesslogic.net>
2024-11-12 12:54:35 +0100

    Fixup

--- src/dwm.h
+++ src/dwm.h
@@ -9,7 +9,7 @@
 // monitor
 static Monitor *createmon(void);                        // create Monitor struct
 static Monitor *dirtomon(int dir);                      // get monitor in direction?
-static Monitor *recttomon(Area *area);  // return the monitor given rect should be rendered on (TODO: use area struct)
+static Monitor *recttomon(Area *area);                  // return the monitor given rect should be rendered on
 static void focusmon(const Arg *arg);                   // focus monitor by index
 static void arrange(Monitor *monitor);                  // arrange specified, or all monitors if m = NULL
 static void arrangemon(Monitor *monitor);               // set layout string and call arrange callback
