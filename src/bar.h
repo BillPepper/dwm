501bd55942dc0574cf55d72a2c447341757a72f5
Mattes Wenzel <mattes@flawlesslogic.net>
2024-11-21 18:41:33 +0100

    Move bar functions to bar.h

---
+++ src/bar.h
@@ -0,0 +1,14 @@
+#ifndef BAR_H
+#define BAR_H
+
+#include "definitions.h"
+
+static void togglebar(const Arg *arg);                                          // toggle bar (arg unused)
+static void update_bar_position(Monitor *monitor);                              // recalculate bar position
+static void drawbar(Monitor *monitor);                                          // draw bar, tags, layout and title
+static void drawbars(void);                                                     // wrapper calling drawbar() for all monitors
+static void resizebarwin(Monitor *monitor);                                     // resize bar and tray
+static void updatestatus(void);                                                 // render current status text or fallback then update tray
+static void updatebars(void);                                                   // set bar class hints, then render bar (top most) for each monitor
+
+#endif
