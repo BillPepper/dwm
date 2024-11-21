704caa6cfc9304c5f8e7edeac64d41c17cab901a
Mattes Wenzel <mattes@flawlesslogic.net>
2024-11-21 18:14:30 +0100

    Move monitor functions to monitor.h

--- src/dwm.h
+++ src/dwm.h
@@ -5,16 +5,7 @@
 #include <X11/Xlib.h>
 
 /* function declarations */
-
-// monitor
-static Monitor *createmon(void);                        // create Monitor struct
-static Monitor *dirtomon(int dir);                      // get monitor in direction?
-static Monitor *recttomon(Area *area);                  // return the monitor given rect should be rendered on
-static void focusmon(const Arg *arg);                   // focus monitor by index
-static void arrange(Monitor *monitor);                  // arrange specified, or all monitors if m = NULL
-static void arrangemon(Monitor *monitor);               // set layout string and call arrange callback
-static void cleanupmon(Monitor *monitor);               // remove monitor
-
+#include "monitor.h"
 #include "layout.h"
 
 // events
