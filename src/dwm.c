bef8eac09fec06acf0203a232e9746cbe7586666
Mattes Wenzel <mattes@flawlesslogic.net>
2024-11-22 02:05:58 +0100

    Move includes from dwm.c to dwm.h

--- src/dwm.c
+++ src/dwm.c
@@ -1,38 +1,4 @@
-#include <X11/Xatom.h>
-#include <X11/Xlib.h>
-#include <X11/Xproto.h>
-#include <X11/Xutil.h>
-#include <X11/cursorfont.h>
-#include <X11/keysym.h>
-
-#include <stdio.h>
-#include <unistd.h>
-#include <locale.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <sys/types.h>
-#include <sys/wait.h>
-
-#ifdef XINERAMA
-#include <X11/extensions/Xinerama.h>
-#endif /* XINERAMA */
-
-#include <X11/Xft/Xft.h>
-
-#include "drw.h"
-#include "util.h"
-
-#include "macro.h"
-
-#include "definitions.h"
-
 #include "dwm.h"
-#include "globals.h"
-
-/* configuration, allows nested code to access above variables */
-#include "../config.h"
 
 /* compile-time check if all tags fit into an unsigned int bit array. */
 struct NumTags {
--- src/dwm.h
+++ src/dwm.h
@@ -1,8 +1,38 @@
 #ifndef DWM_H
 #define DWM_H
 
-#include "definitions.h"
+// X11
+#include <X11/Xatom.h>
+#include <X11/Xlib.h>
+#include <X11/Xproto.h>
+#include <X11/Xutil.h>
+#include <X11/cursorfont.h>
+#include <X11/keysym.h>
+#include <X11/Xft/Xft.h>
 
+// stdlib
+#include <stdio.h>
+#include <unistd.h>
+#include <locale.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+
+// kernel
+#include <sys/types.h>
+#include <sys/wait.h>
+
+// XINERAMA
+#ifdef XINERAMA
+#include <X11/extensions/Xinerama.h>
+#endif
+
+//dwm
+#include "drw.h"
+#include "util.h"
+#include "macro.h"
+#include "definitions.h"
+#include "globals.h"
 #include "core.h"
 #include "monitor.h"
 #include "layout.h"
@@ -16,5 +46,7 @@
 #include "client.h"
 #include "window.h"
 #include "error.h"
+
+#include "../config.h"
 
 #endif
